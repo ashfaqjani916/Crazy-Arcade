df976076cfc87f222b5fb135943211bb81839c5d
ashfaqjani916 <ashfaqjani916@gmail.com>
2023-08-20 01:53:12 +0530

    Added object spawner

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -4,15 +4,53 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public List <GameObject> prop;
+    private Collider spawnArea;
+
+    public float minDelay=0.25f,maxDelay=1f;
+
+    public float minAngle=-15f,maxAngle=15f;
+
+    public float minForce=12f,maxForce=15f;
+
+    public float life = 5f;
+
+    private void Awake()
     {
-        
+        spawnArea = GetComponent<Collider>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        StartCoroutine(Spawn());
     }
+
+    private void OnDisable(){
+        StopAllCoroutines();
+    }
+
+    private IEnumerator Spawn()
+    {
+         yield return new WaitForSeconds(2f);
+        while(enabled)
+        {
+            GameObject obj = prop[Random.Range(0,2)];
+            Vector3 position = new Vector3();
+            position.x = Random.Range(spawnArea.bounds.min.x, spawnArea.bounds.max.x);
+            position.y = Random.Range(spawnArea.bounds.min.y, spawnArea.bounds.max.y);
+            position.z= Random.Range(spawnArea.bounds.min.z, spawnArea.bounds.max.z);
+
+            Quaternion rotation = Quaternion.Euler(0f,0f,Random.Range(minAngle,maxAngle));
+
+            GameObject spikey = Instantiate(obj,position,rotation);
+            Destroy(spikey,life);
+
+            float force = Random.Range(minForce,maxForce);
+            spikey.GetComponent<Rigidbody>().AddForce(spikey.transform.up * force, ForceMode.Impulse);
+            spikey.GetComponent<Rigidbody>().AddForce(spikey.transform.forward*-1 * 2, ForceMode.Impulse);
+
+            yield return new WaitForSeconds(Random.Range(minDelay,maxDelay));
+        }
+    }
+
 }
