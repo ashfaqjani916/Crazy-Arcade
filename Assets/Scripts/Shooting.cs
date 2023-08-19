281c0e5f55587488eb3903395bd15faed5244264
ashfaqjani916 <ashfaqjani916@gmail.com>
2023-08-20 02:24:50 +0530

    added shooting feature

--- Assets/Scripts/Shooting.cs
+++ Assets/Scripts/Shooting.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class Shooting : MonoBehaviour
 {
+    Camera cam;
+    public LayerMask mask;
     // Start is called before the first frame update
     void Start()
     {
-        
+        cam = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //draw a ray
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = 100f;
+        mousePos = cam.ScreenToWorldPoint(mousePos);
+        Debug.DrawRay(transform.position,mousePos-transform.position,Color.red);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitinfo;
+            if(Physics.Raycast(ray,out hitinfo,100,mask))
+            {
+                GameObject hitObject = hitinfo.transform.gameObject;
+                Destroy(hitObject);
+            }
+        }
     }
 }
